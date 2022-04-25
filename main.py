96f3439997b0bf1799cf827a1ff56c5f9be7aad1
jesiel364 <jesiel364@gmail.com>
2022-04-25 15:25:21 -0400

    delete, opção de sexo, hora, avatar

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
+from django.forms import DateTimeField
 from flask import Flask, render_template, request, url_for, redirect, flash
 from markupsafe import escape
 import requests
 import json
+import datetime
 
 app = Flask(__name__)
-
+nome = 'jesiel'
+sexo = 'male'
+avatar = requests.get(f'https://avatars.dicebear.com/api/{sexo}/{nome}.png')
+print(avatar.text)
 
 
 @app.route('/')
 def index():
-	data = requests.get('https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users').json()
-
-	return render_template('home/home.html', data=data)
+	requisicao = requests.get('https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users').json()
+	return render_template('home/home.html', data=requisicao)
 
 @app.route('/perfil/<string:perfil_id>', methods=['POST', 'GET'])
 def perfil(perfil_id):
 	perfil_id=perfil_id
-	for key in data:
-		if key['id'] == perfil_id:
-			perfil = key['name']
-	return render_template('perfil.html', perfil=perfil)
+	# for key in data:
+	# 	if key['id'] == perfil_id:
+	# 		perfil = key['name']
+
+	requisicao = requests.put(f'https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users/{perfil_id}').json()
+	for key in requisicao:
+		print(key)
+	return render_template('perfil.html', perfil=requisicao)
 
 @app.route('/pagina_add', methods=['POST', 'GET'])
 def pagina_add():
@@ -31,15 +39,29 @@ def salvar():
 		nome = request.form['nome']
 		email = request.form['email']
 		idade = request.form['idade']
-		print(requests.post('https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users', data=({'name': nome, 'email': email, 'idade': idade})))
-		flash(f'{nome},{email} {idade} anos', 'success')
-		return redirect(url_for('index'))
+		sexo = request.form['sexo']
+		date = datetime.datetime.now().strftime('%d/%m/%Y ás %H:%M')
+		avatar = f'https://avatars.dicebear.com/api/{sexo}/{nome}.png'
+		
+		
+		try:
+			requisicao = requests.post('https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users', data=({'name': nome, 'avatar': avatar, 'email': email, 'idade': idade, 'createdAt': date}))
+			flash(f'{nome}, {avatar}, {email} {idade} anos', 'success')
+			return redirect(url_for('index'))
+		except:
+			flash(f'Não foi possivel adicionar o usuário {nome}', 'warning')
+
 	return render_template('home/home.html')
 
 @app.route('/delete_user/<string:user_id>', methods=['GET'])
 def delete_user(user_id):
-	print(requests.delete('https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users', json=({'id': user_id})))
-	flash(f'{user_id}', 'success')
+	print(user_id)
+	try:
+		requisicao = requests.delete(f'https://625e20a26c48e8761ba572c5.mockapi.io/api/v1/users/{user_id}')
+		flash(f'{user_id} deletado com sucesso.', 'danger')
+	except:
+		flash(f'{user_id} deletado com sucesso.', 'warning')
+
 	return redirect(url_for('index'))
 
 
